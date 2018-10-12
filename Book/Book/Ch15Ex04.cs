e113f1ed3056ccf9fab3b20c555dd5c1bcd2fe1e
Zhushuai <Zhushuai@DESKTOP-9OI167O>
2018-10-12 15:10:43 +0800

    end of interface

--- Book/Book/Ch15Ex04.cs
+++ Book/Book/Ch15Ex04.cs
@@ -1,27 +1,34 @@
-﻿using System;
+﻿//using System;
 
-namespace Book
-{
-    interface IIfc1
-    {
-        void PrintOut(string s);
-    }
+//namespace Book
+//{
+//    interface IIfc1
+//    {
+//        void PrintOut(string s);
+//    }
 
-    class MyClass:IIfc1
-    {
-        public void PrintOut(string s)
-        {
-            Console.WriteLine("Calling through: {0}", s);
-        }
-    }
-    class Ch15Ex03
-    {
-        static void Main()
-        {
-            MyClass mc = new MyClass();
-            mc.PrintOut("object");
+//    class MyClass:IIfc1
+//    {
+//        public void PrintOut(string s)
+//        {
+//            Console.WriteLine("Calling through: {0}", s);
+//        }
+//    }
+//    class Ch15Ex03
+//    {
+//        static void Main()
+//        {
+//            MyClass mc = new MyClass();
+//            Print(mc, "zhushuai");
+//            //IIfc1 Iint = (IIfc1)mc;
+//            IIfc1 Iint = mc as IIfc1;
+//            Iint.PrintOut("object");
 
-            Console.ReadKey();
-        }
-    }
-}
+//            Console.ReadKey();
+//        }
+//        static void Print(IIfc1 i1, string s)
+//        {
+//            i1.PrintOut(s);
+//        }
+//    }
+//}
