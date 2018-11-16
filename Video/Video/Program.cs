54ded549388101af2c039f8e36812f9691558ca2
ZhuShuai <you@example.com>
2018-11-16 09:46:46 +0800

    20181116

--- Video/Video/Program.cs
+++ Video/Video/Program.cs
@@ -9,13 +9,41 @@ using System.Collections;
 
 namespace Video
 {
+    class  A
+    {
+        public A(int a)
+        {
+            this.a = a;
+        }
+        public int a;
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            string reverse, palindrome;
+            char[] temp;
+
+            Console.WriteLine("Enter a palindrome:");
+            palindrome = Console.ReadLine();
+            reverse = palindrome.Replace(" ", ",");
+            temp = reverse.ToCharArray();
+
+            Array.Reverse(temp);
+
+            if(reverse == new string(temp))
+            {
+                Console.WriteLine($"\"{palindrome}\" is a palindrome.");
+            }
+            else
+                Console.WriteLine($"\"{palindrome}\" is not a palindrome.");
+
+            string str = new string('1', '2');
+            Console.WriteLine(str);
+            decimal d1 = 0.123456789123456789M;
+            Console.WriteLine(d1);
 
             Console.ReadKey();
         }
-
     }
 }
